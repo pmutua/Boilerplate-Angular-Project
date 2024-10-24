be75386e9e8eab554115672f52f3584e4a698d11
Philip Mutua <pmutua@live.com>
2024-10-24 15:27:58 +0300

    feat: add error-messages.constants

---
+++ src/app/core/constants/error-messages.constants.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+    DEFAULT: 'An unexpected error occurred.',
+    NETWORK: 'Network error. Please try again later.',
+    NOT_FOUND: 'Requested resource not found.',
+};
