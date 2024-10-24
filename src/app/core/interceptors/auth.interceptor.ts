590aa2bf5b5548c015936154d193a78d56cb0167
Philip Mutua <pmutua@live.com>
2024-10-24 16:00:02 +0300

    doc(auth.interceptor): add comments

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+/**
+Purpose: Automatically adds authentication tokens (e.g., JWT) to outgoing requests.
+Importance: Essential for maintaining security by ensuring that only authenticated users can access protected resources.
+*/
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
