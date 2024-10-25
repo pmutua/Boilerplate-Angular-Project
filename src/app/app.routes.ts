64442fb0e0f52e781c223bbea1a278d4d0c66549
Philip Mutua <pmutua@live.com>
2024-10-25 06:32:21 +0300

    feat: add lazy loaded pages routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  { path: '', redirectTo: '/pages', pathMatch: 'full' },
+  { path: '**', redirectTo: '/pages' }
+];
