fe65f519e032ff188db3c26a13dc90f9666be702
Philip Mutua <pmutua@live.com>
2024-10-25 06:24:47 +0300

    feat: add exports core services

---
+++ src/app/core/services/index.ts
@@ -0,0 +1,6 @@
+export *  from './auth.service';
+export * from './error-handler.service';
+export * from './loading.service';
+export * from './local-storage-cache.service';
+export * from './logger.service';
+export * from './notification.service';
