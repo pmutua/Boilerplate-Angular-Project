608bea1cea15a6533a0713ff3481225cdc3ff3cb
Philip Mutua <pmutua@live.com>
2024-10-25 06:46:16 +0300

    feat: add auth routes and auth module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // feature based routes must be on top of the pages routes
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
   {
     path: '',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   },
+
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: '/pages' }
 ];
