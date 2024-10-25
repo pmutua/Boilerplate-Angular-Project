f67df29374a1d02fb7973cc42fca1b2b2b209322
Philip Mutua <pmutua@live.com>
2024-10-25 06:26:11 +0300

    refactor: logging interceptor implementation

--- src/app/core/interceptors/logging.interceptor.ts
+++ src/app/core/interceptors/logging.interceptor.ts
@@ -1,16 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoggerService } from '@app/core/services/logger.service';
+
 /**
-Purpose: Logs details of requests and responses.
-Importance: Facilitates tracking of API interactions for easier troubleshooting.
-/
+ * Purpose: Logs details of requests and responses.
+ * Importance: Facilitates tracking of API interactions for easier troubleshooting.
+ */
 @Injectable()
 export class LoggingInterceptor implements HttpInterceptor {
+  constructor(private logger: LoggerService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Request:', req);
+    // Log the request using LoggerService
+    // Adjust to log relevant info
+    this.logger.info(`Request: ${req.method} ${req.urlWithParams}`)
+
     return next.handle(req).pipe(
-      tap(event => console.log('Response:', event))
+      tap(event => {
+        // Log the response using LoggerService
+        this.logger.info(`Response: ${event}`);
+      })
     );
   }
 }
