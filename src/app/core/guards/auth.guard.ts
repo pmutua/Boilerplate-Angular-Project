cd1bc64197de06a307676565af72168d45e60163
Philip Mutua <pmutua@live.com>
2024-11-04 06:51:07 +0300

    feat(guard): add auth guard implementation

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { SupabaseService } from '../services/superbase.service';
 
 /**
  * AuthGuard to protect routes from unauthorized access.
@@ -9,7 +10,9 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root',
 })
 export class AuthGuard {
-  constructor(private authService: AuthService, private router: Router) {}
+  //FIXME: add specific type
+  session: any;
+  constructor(private supabaseService: SupabaseService, private router: Router) {}
 
   /**
    * Check if the user is logged in and can activate the route.
@@ -24,10 +27,10 @@ export class AuthGuard {
     state: RouterStateSnapshot
   ): boolean => {
     // Check if the user is logged in
-    if (this.authService.isLoggedIn()) {
+    this.supabaseService.authChanges((_, session) => (this.session = session))
+    if (this.session) {
       return true; // Allow access
     }
-
     // Redirect to login if not authenticated
     // queryParams = This allows the application to remember where the user intended to go before being redirected
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
