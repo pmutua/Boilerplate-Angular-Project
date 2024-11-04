5504e9a9689af73a47ffd0f4318dd364ffb6dfd6
Philip Mutua <pmutua@live.com>
2024-11-04 06:50:43 +0300

    feat: add reactive forms module in the auth module

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
